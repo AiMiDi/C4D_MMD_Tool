c7a05ce49d4db883bd080039c418de92ee8cb42b
Aimidi <ai.mi.di@qq.com>
2026-03-02 03:09:22 +0800

    update: SDK2024

--- sdk_2024/plugins/mmdtool/generated/hxx/register.hxx
+++ sdk_2024/plugins/mmdtool/generated/hxx/register.hxx
@@ -17,7 +17,7 @@
 
 #else
 
-static const maxon::LiteralId g_usedFrameworks[] = {maxon::LiteralId("net.maxon.c4d.cinema.framework"), maxon::LiteralId("net.maxon.core.framework"), maxon::LiteralId("net.maxon.image.framework"), maxon::LiteralId("net.maxon.math.framework"), maxon::LiteralId("net.maxon.mesh_misc.framework"), maxon::LiteralId("net.maxon.modeling_geom.framework")};
+static const maxon::LiteralId g_usedFrameworks[] = {maxon::LiteralId("net.maxon.asset.framework"), maxon::LiteralId("net.maxon.c4d.cinema.framework"), maxon::LiteralId("net.maxon.core.framework"), maxon::LiteralId("net.maxon.graph.framework"), maxon::LiteralId("net.maxon.image.framework"), maxon::LiteralId("net.maxon.math.framework"), maxon::LiteralId("net.maxon.mesh_misc.framework"), maxon::LiteralId("net.maxon.misc.framework"), maxon::LiteralId("net.maxon.modeling_geom.framework"), maxon::LiteralId("net.maxon.nodes.framework"), maxon::LiteralId("net.maxon.nodespace.framework")};
 static const maxon::SetUsedFrameworks g_setUsedFrameworks{PRIVATE_MAXON_MODULE, g_usedFrameworks};
 
 static maxon::TranslationUnit g_reflectionRegistrationUnit{&PRIVATE_MAXON_MODULE, "[reflection registration]", maxon::EntityBase::FLAGS::SYNTHETIC | maxon::EntityBase::FLAGS::REFLECTION_REGISTRATION};
